3733151b2a5be838b88602fdcc84fc63c2b2f150
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-20 12:37:05 +0100

    aggiunto progetto base spring boot

---
+++ iceTimeApp/src/test/java/com/example/icetime/iceTimeApp/IceTimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icetime.iceTimeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IceTimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
