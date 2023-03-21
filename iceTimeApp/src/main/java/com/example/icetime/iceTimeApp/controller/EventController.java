c249d478cb7b499b10f6a1eadf8e8f7d13852ddc
Nicola Caddeo <85455970+nicolacaddeo@users.noreply.github.com>
2023-03-21 14:48:58 +0100

    Aggiornato EventController

--- iceTimeApp/src/main/java/com/example/icetime/iceTimeApp/controller/EventController.java
+++ iceTimeApp/src/main/java/com/example/icetime/iceTimeApp/controller/EventController.java
@@ -2,53 +2,57 @@ package com.example.icetime.iceTimeApp.controller;
 
 import com.example.icetime.iceTimeApp.entity.Event;
 import com.example.icetime.iceTimeApp.service.EventService;
-
+import com.example.icetime.iceTimeApp.entity.User;
+import com.example.icetime.iceTimeApp.service.UserService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@RestController
+@Controller
 public class EventController {
+
+    private UserService userService;
+
+    public EventController(UserService userService) {
+        this.userService = userService;
+    }
 
     @Autowired
     EventService eventService;
 
-    // * recupero di tutti gli eventi
-    @GetMapping("/events")
-    private List<Event> getAllEvents() {
-        return eventService.getAllEvents();
-    }
+    // * GET per il form di creazione
+    @GetMapping("/events/create")
+    public String createEvent(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        String email = userDetails.getUsername();
+        User user = userService.findUserByEmail(email);
+        model.addAttribute("user", user);
+        model.addAttribute("event", new Event());
 
-    // * recupero di uno specifico evento
-    @GetMapping("/events/{id}")
-    private Event getEvents(@PathVariable("id") Long id) {
-        return eventService.getEventById(id);
-    }
-
-    // * delete di un evento
-    @DeleteMapping("/events/{id}")
-    private void deleteEvent(@PathVariable("id") Long id) {
-        eventService.delete(id);
+        return "/events/create";
     }
 
     // * salvataggio nel DB di un evento
-    @PostMapping("/events/create")
-    private Long saveEvent(@RequestBody Event event) {
-        eventService.saveOrUpdate(event);
-        return event.getId();
+    @PostMapping("/events/createevent")
+    public String store(@AuthenticationPrincipal UserDetails userDetails, @ModelAttribute("event") Event formEvent,
+            Model model) {
+        String email = userDetails.getUsername();
+        User user = userService.findUserByEmail(email);
+        formEvent.setUser(user);
+        eventService.saveOrUpdate(formEvent);
+
+        return "redirect:/calendar";
     }
 
-    // * modifica di un evento
-    @PutMapping("/events/update")
-    private Event update(@RequestBody Event event) {
-        eventService.saveOrUpdate(event);
-        return event;
-    }
 }
